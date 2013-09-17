4a880c79a50685b5e6fdab18ff379a730a3fc752
Guillaume LAURES <ice.guillaume@gmail.com>
2013-09-17 16:19:46 +0200

    initial commit

---
+++ src/com/wififileexchange/MainActivity.java
@@ -0,0 +1,17 @@
+package com.wififileexchange;
+
+import com.actionbarsherlock.app.SherlockListActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends SherlockListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    
+}
